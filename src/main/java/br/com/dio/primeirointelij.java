ca89895c45df7fbf3027a75a658828400f3ed633
Emilio <040emilio@gmail.com>
2022-05-19 16:25:57 -0300

    First commit2

--- src/main/java/br/com/dio/primeirointelij.java
+++ src/main/java/br/com/dio/primeirointelij.java
@@ -3,11 +3,20 @@ package br.com.dio;
 import br.com.dio.model.Gato;
 
 public class primeirointelij {
+    public primeirointelij() {
+    }
+
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
-//      /*  int a = 5;
-//        int b = 3;
-//        System.out.println("Hello World! " + (a+b));
-    }
-}
+
+        Livro livro = new Livro( nome: "o problema dos 3 corpos", numPaginas 300);
+
+
+class Livro{
+    private String nome;
+    private Integer numPaginas;
+
+
+
+
